4cdb39aeec91d8e7eee8d0ef4f03f3b89eb44673
Alireza Salary <alireza.salary@outlook.com>
2022-04-19 23:35:42 +0300

    add handler

---
+++ swagger.go
@@ -0,0 +1,69 @@
+package swagger
+
+import (
+	"fmt"
+	"html/template"
+	"path"
+	"strings"
+	"sync"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/utils"
+	swaggerFiles "github.com/swaggo/files"
+	"github.com/swaggo/swag"
+)
+
+const (
+	defaultDocURL = "doc.json"
+	defaultIndex  = "index.html"
+)
+
+var (
+	HandlerDefault = New()
+)
+
+// New returns custom handler
+func New(config ...Config) fiber.Handler {
+	cfg := configDefault(config...)
+
+	index, err := template.New("swagger_index.html").Parse(indexTmpl)
+	if err != nil {
+		panic(fmt.Errorf("Fiber: swagger middleware error -> %w", err))
+	}
+
+	var (
+		prefix string
+		once   sync.Once
+		fs     fiber.Handler = filesystem.New(filesystem.Config{Root: swaggerFiles.HTTP})
+	)
+
+	return func(c *fiber.Ctx) error {
+		// Set prefix
+		once.Do(func() {
+			prefix = strings.ReplaceAll(c.Route().Path, "*", "")
+			// Set doc url
+			if len(cfg.URL) == 0 {
+				cfg.URL = path.Join(prefix, defaultDocURL)
+			}
+		})
+
+		p := c.Path(utils.ImmutableString(c.Params("*")))
+
+		switch p {
+		case defaultIndex:
+			c.Type("html")
+			return index.Execute(c, cfg)
+		case defaultDocURL:
+			doc, err := swag.ReadDoc()
+			if err != nil {
+				return err
+			}
+			return c.Type("json").SendString(doc)
+		case "", "/":
+			return c.Redirect(path.Join(prefix, defaultIndex), fiber.StatusMovedPermanently)
+		default:
+			return fs(c)
+		}
+	}
+}
