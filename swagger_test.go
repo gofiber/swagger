0c0abfa27340f3f02c2b77572393aa9fb528db19
Alireza Salary <alireza.salary@outlook.com>
2022-04-20 01:51:22 +0300

    Update tests

--- swagger_test.go
+++ swagger_test.go
@@ -5,11 +5,39 @@ import (
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/gofiber/swagger/example/docs"
+	"github.com/swaggo/swag"
 )
 
+type mockedSwag struct{}
+
+func (s *mockedSwag) ReadDoc() string {
+	return `{
+    "swagger": "2.0",
+    "info": {
+        "description": "This is a sample server.",
+        "title": "Swagger Example API",
+        "termsOfService": "http://swagger.io/terms/",
+        "contact": {
+            "name": "API Support",
+            "url": "http://www.swagger.io/support",
+            "email": "support@swagger.io"
+        },
+        "license": {
+            "name": "Apache 2.0",
+            "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+        },
+        "version": "1.0"
+    },
+    "host": "petstore.swagger.io",
+    "basePath": "/v2",
+    "paths": {}
+}`
+}
+
 func Test_Swagger(t *testing.T) {
-	app := *fiber.New()
+	app := fiber.New()
+
+	swag.Register(swag.Name, &mockedSwag{})
 
 	app.Get("/swag/*", HandlerDefault)
 
@@ -51,20 +79,24 @@ func Test_Swagger(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			req, _ := http.NewRequest("GET", tt.url, nil)
+			req, err := http.NewRequest(http.MethodGet, tt.url, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
 			resp, err := app.Test(req)
 			if err != nil {
-				t.Fatalf(`%s: %s`, t.Name(), err)
+				t.Fatal(err)
 			}
 
 			if resp.StatusCode != tt.statusCode {
-				t.Fatalf(`%s: StatusCode: got %v - expected %v`, t.Name(), resp.StatusCode, tt.statusCode)
+				t.Fatalf(`StatusCode: got %v - expected %v`, resp.StatusCode, tt.statusCode)
 			}
 
 			if tt.contentType != "" {
 				ct := resp.Header.Get("Content-Type")
 				if ct != tt.contentType {
-					t.Fatalf(`%s: Content-Type: got %s - expected %s`, t.Name(), ct, tt.contentType)
+					t.Fatalf(`Content-Type: got %s - expected %s`, ct, tt.contentType)
 				}
 			}
 		})
